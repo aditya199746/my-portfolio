eca768763b462b14e9df3db42661cb481acf6fec
Aditya Kumar <461997adityakumar@gmail.com>
2023-11-04 00:58:29 +0530

    complete portfolio

---
+++ src/content_option.js
@@ -0,0 +1,183 @@
+const logotext = "Aditya";
+const meta = {
+    title: "Aditya Kumar",
+    description: "I’m Aditya Kumar Frontend Engineer and Full Stack Engineer,currently working in Mashreq (Fragma Data System)",
+};
+
+const introdata = {
+    title: "I’m Aditya Kumar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I understand Banking Domain (Mashreq Bank Dubai)"
+    },
+    description: `Highly skilled and innovative Front-End Engineer with Half a decade of experience in designing 
+    and developing user- centric web applications.Proficient in HTML, CSS, JavaScript and Typescript, with a strong 
+    understanding of responsive design and Banking principles and cross-browser compatibility.Seeking for opportunities and a 
+    challenging role to contribute my expertise in front-end development.`,
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: `Highly skilled and innovative Front-End Engineer with Half a decade of experience in designing 
+    and developing user- centric web applications.Proficient in HTML, CSS, JavaScript and Typescript, with a strong 
+    understanding of responsive design and Banking principles and cross-browser compatibility.Seeking for opportunities and a 
+    challenging role to contribute my expertise in front-end development.`,
+};
+const worktimeline = [{
+    jobtitle: "Software Engineer",
+    where: "Mashreq (Fragma Data System)",
+    date: "2022",
+},
+{
+    jobtitle: "Senior Associate UI-Platforms",
+    where: "Magic EdTech",
+    date: "2021",
+},
+{
+    jobtitle: "System Engineer",
+    where: "Nationwide (Tata Consultancy Services)",
+    date: "2018",
+},
+];
+
+const skills = [{
+    name: "React JS",
+    value: 90,
+},
+{
+    name: "Node JS",
+    value: 85,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+{
+    name: "TypeScript",
+    value: 90,
+},
+{
+    name: "Jquery",
+    value: 80,
+},
+{
+    name: "Redux",
+    value: 80,
+},
+{
+    name: "Mongo DB",
+    value: 60,
+},
+{
+    name: "MYsql",
+    value: 60,
+},
+];
+
+const services = [
+    //{
+    //     title: "UI & UX Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+    // {
+    //     title: "Mobile Apps",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+    // {
+    //     title: "Wordpress Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "461997adityakumar@gmail.com",
+    YOUR_FONE: "(+91)8755511349",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_kk48h39",
+    YOUR_TEMPLATE_ID: "template_hdyvj9r",
+    YOUR_USER_ID: "qqGjeZHwQGTyk6ce5",
+};
+
+const socialprofils = {
+    github: "https://github.com/aditya199746",
+    linkedin: "https://www.linkedin.com/in/aditya-kumar-961105185/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
